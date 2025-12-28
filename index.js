9f5ce461100a422d9d6d33444e798083e2121e34
Pedram <pedramsoofi064@gmail.com>
2025-12-28 21:04:11 +0200

    feat: add client

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const morgan = require("morgan");
 const app = express();
 app.use(express.json());
+app.use(express.static('dist'))
 
 morgan.token("req-body", function (req, res) {
   if (!req.body) return "";
